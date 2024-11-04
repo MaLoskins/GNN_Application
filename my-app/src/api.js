f63e71c2670605d36ae3a21f6aa5b776fcca9630
MaLoskins <matthew.haskins.mh@gmail.com>
2024-11-04 21:48:52 +0800

    Reset to backup

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,4 +1,4 @@
-// src/api.js
+// my-app/src/api.js
 import axios from 'axios';
 
 const API_BASE_URL = 'http://127.0.0.1:8000';
@@ -12,9 +12,9 @@ export const getRoot = async () => {
   }
 };
 
-export const processData = async (data) => {
+export const processData = async (data, config) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/process-data`, { data });
+    const response = await axios.post(`${API_BASE_URL}/process-data`, { data, config });
     return response.data;
   } catch (error) {
     throw error;
